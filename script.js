c274ad398a51682ec70fd123bee419cf6430eb91
Jorge Samper <60151922+jorgesamper@users.noreply.github.com>
2024-01-23 13:59:28 +0000

    cursor

--- script.js
+++ script.js
@@ -1,6 +1,149 @@
 
 AOS.init();
 
+//cursor
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const cursor = document.getElementById("cursor");
+  
+//     let position = {
+//       distanceX: 0,
+//       distanceY: 0,
+//       pointerX: 0,
+//       pointerY: 0,
+//     };
+  
+//     let previousPointerX = 0;
+//     let previousPointerY = 0;
+//     let angle = 0;
+//     let previousAngle = 0;
+//     let angleDisplace = 0;
+//     const degrees = 57.296;
+//     let moving = false;
+  
+//     let cursorSize = 0;
+//     let cursorSizeInit = 0;
+  
+//     // The cursor status of the default cursor visibility
+//     setOriginalCursor("none");
+  
+//     /**
+//      * The cursor size from the CSS variable
+//      */
+//     cursorSizeInit = cursorSize =
+//       Number(getComputedStyle(cursor).getPropertyValue("--size").slice(0, -2)) / 2;
+  
+//     /**
+//      * Center the position of cursor after its container loaded
+//      */
+//     init();
+  
+//     /**
+//      * Get the cursor position by event and apply them to the transform property of the cursor
+//      *
+//      * @param {object} event
+//      * @param {object} cursorBlock
+//      */
+//     function move(event, cursorBlock) {
+//       previousPointerX = position.pointerX;
+//       previousPointerY = position.pointerY;
+//       position.pointerX = event.pageX - cursorBlock.getBoundingClientRect().x;
+//       position.pointerY =
+//         event.pageY -
+//         cursorBlock.getBoundingClientRect().y +
+//         document.documentElement.getBoundingClientRect().y;
+//       position.distanceX = Math.min(
+//         Math.max(previousPointerX - position.pointerX, -20),
+//         20
+//       );
+//       position.distanceY = Math.min(
+//         Math.max(previousPointerY - position.pointerY, -20),
+//         20
+//       );
+  
+//       cursor.style.transform = `translate3d(${position.pointerX}px, ${position.pointerY}px, 0)`;
+//       rotate(position);
+//       moving ? stop() : (moving = true);
+//     }
+  
+//     function rotate(position) {
+//       let unsortedAngle =
+//         (Math.atan(Math.abs(position.distanceY) / Math.abs(position.distanceX)) *
+//           degrees);
+  
+//       if (isNaN(unsortedAngle)) {
+//         angle = previousAngle;
+//       } else {
+//         if (unsortedAngle <= 45) {
+//           if (position.distanceX * position.distanceY >= 0) {
+//             angle = +unsortedAngle;
+//           } else {
+//             angle = -unsortedAngle;
+//           }
+//         } else {
+//           if (position.distanceX * position.distanceY <= 0) {
+//             angle = 180 - unsortedAngle;
+//           } else {
+//             angle = unsortedAngle;
+//           }
+//         }
+//       }
+//       cursor.style.transform += ` rotate(${angle}deg)`;
+//       previousAngle = angle;
+//     }
+  
+//     function stop() {
+//       setTimeout(() => {
+//         moving = false;
+//       }, 50);
+//     }
+  
+//     function setOriginalCursor(value) {
+//       cursor.parentElement.style.cursor = value;
+//       cursor.parentElement
+//         .querySelectorAll(
+//           "button, label, input, textarea, select, a"
+//         )
+//         .forEach((el) => {
+//           el.style.cursor = "inherit";
+//         });
+//     }
+  
+//     /**
+//      * Center the position of cursor when leaving its container
+//      */
+//     function reset() {
+//       cursor.style.top = "";
+//       cursor.style.left = "";
+//       cursor.style.transform = "";
+//       cursor.style.transition = "500ms";
+//     }
+  
+//     function init() {
+//       cursor.style.top = `${(
+//         getComputedStyle(cursor).getPropertyValue("--size").slice(0, -2) / -2
+//       )}px`;
+//       cursor.style.left = `${(
+//         getComputedStyle(cursor).getPropertyValue("--size").slice(0, -2) / -2
+//       )}px`;
+//       cursor.style.transition = "";
+//       cursor.addEventListener("click", click);
+//     }
+  
+//     function click() {
+//       // Aquí puedes manejar la lógica del clic del cursor
+//     }
+  
+//     // Puedes agregar otros manejadores de eventos o lógica según tus necesidades
+  
+//     document.addEventListener("mousemove", (e) => {
+//       move(e, cursor);
+//     });
+  
+//     document.addEventListener("mouseleave", reset);
+//     document.addEventListener("mouseenter", init);
+//   });
+
 // navbar out scroll  
 
 let lastScrollTop = 0;
